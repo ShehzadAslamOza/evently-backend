2b52a1049b91eb0a7bb6dfdb2e1a9501f19eacd4
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-06-19 04:20:08 +0500

    Completed logout Token functionality

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,8 +1,28 @@
 const asyncHandler = require("express-async-handler");
 const { StatusCodes } = require("http-status-codes");
+const User = require("../../models/User");
 
 const logout = asyncHandler(async (req, res, next) => {
-  res.status(StatusCodes.OK).json({ msg: "in logout" });
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    return res.sendStatus(StatusCodes.NO_CONTENT);
+  } // No Content
+
+  const refreshToken = cookies.jwt;
+
+  const foundUser = await User.findOne({ refreshToken: refreshToken }).exec();
+
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true });
+    return res.sendStatus(StatusCodes.NO_CONTENT);
+  }
+
+  // If refresh token in db
+  foundUser.refreshToken = "";
+  await foundUser.save();
+
+  res.clearCookie("jwt", { httpOnly: true });
+  res.sendStatus(StatusCodes.NO_CONTENT);
 });
 
 module.exports = {
