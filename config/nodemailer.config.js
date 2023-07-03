3bee4d6e22d2c0001dcd606153526206efe886f2
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-07-03 13:55:51 +0500

    Fixed the issue with nodemailer

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,58 +1,78 @@
+const axios = require("axios");
 const nodemailer = require("nodemailer");
 
 const user = process.env.MAIL_USER;
 const pass = process.env.MAIL_PWD;
 
-const transport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: user,
-    pass: pass,
-  },
-});
+// const transport = nodemailer.createTransport({
+//   service: "Gmail",
+//   auth: {
+//     user: user,
+//     pass: pass,
+//   },
+// });
 
-const sendConfirmationEmail = async (email, name, confirmationCode) => {
+const sendConfirmationEmail = (email123, name, confirmationCode) => {
   console.log("Check");
-  await new Promise((resolve, reject) => {
-    transport.sendMail(
-      {
-        from: user,
-        to: email,
-        subject: "Please confirm your account",
-        html: `<h1>Email Confirmation</h1>
-          <h2>Hello ${name}</h2>
-          <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
-          <a href=https://evently-backend.vercel.app/auth/confirm/${confirmationCode}> Click Here</a>
-          </div>`,
-      },
-      (err, info) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log(info);
-          resolve(info);
-        }
-      }
-    );
-  });
-};
 
-const resetEmail = (email, name, resetCode) => {
-  console.log("Check");
-  transport
-    .sendMail({
-      from: user,
-      to: email,
-      subject: "Please reset your password",
-      html: `<h1>Reset Password</h1>
-            <h2>Hello ${name}</h2>
-            <p>Please reset your password by clicking the link</p>
-            <a href=http://localhost:3001/auth/confirm/${resetCode}> Click Here</a>
-            </div>`,
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email123, // Change to your recipient
+    from: user, // Change to your verified sender
+    subject: "Please confirm your account",
+    text: "Thank you for subscribing. Please confirm your email by clicking on the following link",
+    html: "<a href=https://evently-backend.vercel.app/auth/confirm/${confirmationCode}> Click Here</a>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
     })
-    .catch((err) => console.log(err));
+    .catch((error) => {
+      console.error(error);
+    });
+
+  // await new Promise((resolve, reject) => {
+  // transport.sendMail(
+  //   {
+  //     from: user,
+  //     to: email,
+  //     subject: "Please confirm your account",
+  //     html: `<h1>Email Confirmation</h1>
+  //       <h2>Hello ${name}</h2>
+  //       <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+  //       <a href=https://evently-backend.vercel.app/auth/confirm/${confirmationCode}> Click Here</a>
+  //       </div>`,
+  //   },
+  //   (err, info) => {
+  //     if (err) {
+  //       console.error(err);
+  //       reject(err);
+  //     } else {
+  //       console.log(info);
+  //       resolve(info);
+  //     }
+  //   }
+  // );
+  // });
 };
+
+// const resetEmail = (email, name, resetCode) => {
+//   console.log("Check");
+//   transport
+//     .sendMail({
+//       from: user,
+//       to: email,
+//       subject: "Please reset your password",
+//       html: `<h1>Reset Password</h1>
+//             <h2>Hello ${name}</h2>
+//             <p>Please reset your password by clicking the link</p>
+//             <a href=http://localhost:3001/auth/confirm/${resetCode}> Click Here</a>
+//             </div>`,
+//     })
+//     .catch((err) => console.log(err));
+// };
 
 const dailyMail = (email, name) => {
   console.log("Check");
@@ -69,4 +89,4 @@ const dailyMail = (email, name) => {
     .catch((err) => console.log(err));
 };
 
-module.exports = { sendConfirmationEmail, resetEmail, dailyMail };
+module.exports = { sendConfirmationEmail, dailyMail };
